2d0ee24a00092d29fb7f1d8b0aa442c35d13eff6
Mohamed Gaber <me@donn.website>
2022-06-25 03:18:05 +0200

    Update main.swift

--- Sources/Fault/Entries/main.swift
+++ Sources/Fault/Entries/main.swift
@@ -6,7 +6,7 @@ import Defile
 import OrderedDictionary
 import BigInt
 
-let VERSION = "APPIMAGE_TEST3"
+let VERSION = "0.5.0"
 
 var env = ProcessInfo.processInfo.environment
 let iverilogBase = env["FAULT_IVL_BASE"] ?? "/usr/local/lib/ivl"
