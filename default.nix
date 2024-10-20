5eb008b52869c40286afa096879b706e76090f2d
Mohamed Gaber <donn@efabless.com>
2024-10-20 16:20:04 +0300

    Update Nix Dependencies

--- default.nix
+++ default.nix
@@ -9,105 +9,107 @@
   yosys,
   verilog,
   quaigh,
-  nl2bench,
   ncurses,
   makeBinaryWrapper,
-}:
-let
+}: let
   generated = swiftpm2nix.helpers ./nix;
-  pyenv = (python3.withPackages(ps: with ps; [pyverilog]));
+  pyenv = python3.withPackages (ps: with ps; [nl2bench pyverilog]);
   stdenv = swiftPackages.stdenv;
 in
-stdenv.mkDerivation (finalAttrs: {
-	name = "fault";
-	
-	src = nix-gitignore.gitignoreSourcePure ./.gitignore ./.;
-  
-	swiftpmFlags = [
-    "--verbose"
-  ];
-  # ++ lib.lists.optional swiftPackages.stdenv.isDarwin [
-  #   "-Xcc"
-  #   "-mmacosx-version-min=11"
-  #   "-Xcc"
-  #   "-target"
-  #   "-Xcc"
-  #   "x86_64-apple-macosx11"
-  #   "-Xswiftc"
-  #   "-target"
-  #   "-Xswiftc"
-  #   "x86_64-apple-macosx11"
-  # ];
-  nativeBuildInputs = [ swift swiftpm makeBinaryWrapper ];
-  
-  buildInputs = with swiftPackages; [
-    Foundation
-    XCTest
-  ] ++ lib.lists.optional (!stdenv.isDarwin) [Dispatch];
-  
-  propagatedBuildInputs = [
-    pyenv
-    yosys
-    verilog
-    quaigh
-    nl2bench
-  ];
-  
-  nativeCheckInputs = with python3.pkgs; [
-    pytest
-  ];
-  
-  configurePhase = generated.configure;
-  
-  # This doesn't work on Linux otherwise and I don't know why.
-  preBuild = if stdenv.isDarwin then "" else ''
-    export LD_LIBRARY_PATH=${swiftPackages.Dispatch}/lib:$LD_LIBRARY_PATH
-  '';
-  
-  doCheck = true;
-  
-  faultEnv = ''
-    export PYTHONPATH=${pyenv}/${pyenv.sitePackages}
-    export PATH=${verilog}/bin:$PATH
-    export PATH=${yosys}/bin:$PATH
-    export PYTHON_LIBRARY=${pyenv}/lib/lib${pyenv.libPrefix}${swiftPackages.stdenv.hostPlatform.extensions.sharedLibrary}
-    export FAULT_IVL_BASE=${verilog}/lib/ivl
-  '';
-  
-  checkPhase = ''
-    runHook preCheck
-    ${finalAttrs.faultEnv}
-    PYTEST_FAULT_BIN="$(swiftpmBinPath)/fault" pytest
-    runHook postCheck
-  '';
+  stdenv.mkDerivation (finalAttrs: {
+    name = "fault";
 
-  installPhase = ''
-    runHook preInstall
-    binPath="$(swiftpmBinPath)"
-    mkdir -p $out/bin
-    cp $binPath/fault $out/bin/fault
-    ln -s ${nl2bench}/bin/nl2bench $out/bin/nl2bench
-    runHook postInstall
-  '';
-  
-  fixupPhase = ''
-    runHook preFixup
-    wrapProgram $out/bin/fault\
-      --prefix PYTHONPATH : ${pyenv}/${pyenv.sitePackages}\
-      --prefix PATH : ${verilog}/bin\
-      --prefix PATH : ${quaigh}/bin\
-      --prefix PATH : ${yosys}/bin\
-      --set PYTHON_LIBRARY ${pyenv}/lib/lib${pyenv.libPrefix}${swiftPackages.stdenv.hostPlatform.extensions.sharedLibrary}\
-      --set FAULT_IVL_BASE ${verilog}/lib/ivl
-    runHook postFixup
-  '';
-  
-  meta = with lib; {
-    description = "Open-source EDA's missing DFT toolchain";
-    homepage = "https://github.com/AUCOHL/Fault";
-    license = licenses.asl20;
-    platforms = platforms.linux ++ platforms.darwin;
-  };
-  
-  shellHook = finalAttrs.faultEnv + finalAttrs.preBuild;
-})
+    src = nix-gitignore.gitignoreSourcePure ./.gitignore ./.;
+
+    swiftpmFlags = [
+      "--verbose"
+    ];
+    # ++ lib.lists.optional swiftPackages.stdenv.isDarwin [
+    #   "-Xcc"
+    #   "-mmacosx-version-min=11"
+    #   "-Xcc"
+    #   "-target"
+    #   "-Xcc"
+    #   "x86_64-apple-macosx11"
+    #   "-Xswiftc"
+    #   "-target"
+    #   "-Xswiftc"
+    #   "x86_64-apple-macosx11"
+    # ];
+    nativeBuildInputs = [swift swiftpm makeBinaryWrapper];
+
+    buildInputs = with swiftPackages;
+      [
+        Foundation
+        XCTest
+      ]
+      ++ lib.lists.optional (!stdenv.isDarwin) [Dispatch];
+
+    propagatedBuildInputs = [
+      pyenv
+      yosys
+      verilog
+      quaigh
+    ];
+
+    nativeCheckInputs = with python3.pkgs; [
+      pytest
+    ];
+
+    configurePhase = generated.configure;
+
+    # This doesn't work on Linux otherwise and I don't know why.
+    preBuild =
+      if stdenv.isDarwin
+      then ""
+      else ''
+        export LD_LIBRARY_PATH=${swiftPackages.Dispatch}/lib:$LD_LIBRARY_PATH
+      '';
+
+    doCheck = true;
+
+    faultEnv = ''
+      export PYTHONPATH=${pyenv}/${pyenv.sitePackages}
+      export PATH=${verilog}/bin:$PATH
+      export PATH=${yosys}/bin:$PATH
+      export PYTHON_LIBRARY=${pyenv}/lib/lib${pyenv.libPrefix}${swiftPackages.stdenv.hostPlatform.extensions.sharedLibrary}
+      export FAULT_IVL_BASE=${verilog}/lib/ivl
+    '';
+
+    checkPhase = ''
+      runHook preCheck
+      ${finalAttrs.faultEnv}
+      PYTEST_FAULT_BIN="$(swiftpmBinPath)/fault" pytest
+      runHook postCheck
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      binPath="$(swiftpmBinPath)"
+      mkdir -p $out/bin
+      cp $binPath/fault $out/bin/fault
+      ln -s ${python3.pkgs.nl2bench}/bin/nl2bench $out/bin/nl2bench
+      runHook postInstall
+    '';
+
+    fixupPhase = ''
+      runHook preFixup
+      wrapProgram $out/bin/fault\
+        --prefix PYTHONPATH : ${pyenv}/${pyenv.sitePackages}\
+        --prefix PATH : ${verilog}/bin\
+        --prefix PATH : ${quaigh}/bin\
+        --prefix PATH : ${yosys}/bin\
+        --set PYTHON_LIBRARY ${pyenv}/lib/lib${pyenv.libPrefix}${swiftPackages.stdenv.hostPlatform.extensions.sharedLibrary}\
+        --set FAULT_IVL_BASE ${verilog}/lib/ivl
+      runHook postFixup
+    '';
+
+    meta = with lib; {
+      description = "Open-source EDA's missing DFT toolchain";
+      homepage = "https://github.com/AUCOHL/Fault";
+      license = licenses.asl20;
+      platforms = platforms.linux ++ platforms.darwin;
+    };
+
+    shellHook = finalAttrs.faultEnv + finalAttrs.preBuild;
+  })
--- flake.nix
+++ flake.nix
@@ -1,42 +1,77 @@
 {
   inputs = {
     nix-eda.url = github:efabless/nix-eda;
-    nl2bench = {
-      url = github:donn/nl2bench/pyosys;
-      inputs.nix-eda.follows = "nix-eda";
-    };
     quaigh = {
       url = github:coloquinte/quaigh;
       inputs.nixpkgs.follows = "nix-eda/nixpkgs";
     };
+    nl2bench = {
+      url = github:donn/nl2bench;
+      inputs.nix-eda.follows = "nix-eda";
+      inputs.quaigh.follows = "quaigh";
+    };
   };
 
-  outputs = {self, nix-eda, quaigh, nl2bench, ...}: {
-    packages = nix-eda.forAllSystems { current = self; withInputs = [nix-eda quaigh nl2bench]; } (util: with util; rec{
-      atalanta = callPackage ./nix/atalanta.nix {};
-      podem = callPackage ./nix/podem.nix {};
-      fault = callPackage ./default.nix {};
-      default = fault;
+  outputs = {
+    self,
+    nix-eda,
+    quaigh,
+    nl2bench,
+    ...
+  }: let
+    nixpkgs = nix-eda.inputs.nixpkgs;
+    lib = nixpkgs.lib;
+  in {
+    overlays = {
+      default = lib.composeManyExtensions [
+        (nix-eda.flakesToOverlay [quaigh])
+        nl2bench.overlays.default
+        (pkgs': pkgs: let
+          callPackage = lib.callPackageWith pkgs';
+        in {
+          atalanta = callPackage ./nix/atalanta.nix {};
+          podem = callPackage ./nix/podem.nix {};
+          fault = callPackage ./default.nix {};
+        })
+      ];
+    };
+
+    legacyPackages = nix-eda.forAllSystems (
+      system:
+        import nixpkgs {
+          inherit system;
+          overlays = [nix-eda.overlays.default self.overlays.default];
+        }
+    );
+
+    packages = nix-eda.forAllSystems (system: {
+      inherit (self.legacyPackages.${system}) atalanta podem fault;
+      default = self.packages.${system}.fault;
     });
-    
-    devShells = nix-eda.forAllSystems { withInputs = [nix-eda quaigh nl2bench self]; } (util: with util; rec {
-      mac-testing = pkgs.stdenvNoCC.mkDerivation (with pkgs; let
-        pyenv = (python3.withPackages(ps: with ps; [pyverilog pyyaml pytest pkgs.nl2bench]));
+
+    devShells = nix-eda.forAllSystems (
+      system: let
+        pkgs = self.legacyPackages."${system}";
+        callPackage = lib.callPackageWith pkgs;
       in {
-        # Use the host's Clang and Swift
-        name = "shell";
-        buildInputs = [
-          yosys
-          verilog
-          pkgs.quaigh
-          pyenv
-          gtkwave
-        ];
-        
-        PYTHON_LIBRARY="${pyenv}/lib/lib${python3.libPrefix}${stdenvNoCC.hostPlatform.extensions.sharedLibrary}";
-        PYTHONPATH="${pyenv}/${pyenv.sitePackages}";
-        FAULT_IVL_BASE="${verilog}/lib/ivl";
-      });
-    });
+        mac-testing = pkgs.stdenvNoCC.mkDerivation (with pkgs; let
+          pyenv = python3.withPackages (ps: with ps; [pyverilog pyyaml pytest nl2bench]);
+        in {
+          # Use the host's Clang and Swift
+          name = "shell";
+          buildInputs = [
+            yosys
+            verilog
+            pkgs.quaigh
+            pyenv
+            gtkwave
+          ];
+
+          PYTHON_LIBRARY = "${pyenv}/lib/lib${python3.libPrefix}${stdenvNoCC.hostPlatform.extensions.sharedLibrary}";
+          PYTHONPATH = "${pyenv}/${pyenv.sitePackages}";
+          FAULT_IVL_BASE = "${verilog}/lib/ivl";
+        });
+      }
+    );
   };
 }
