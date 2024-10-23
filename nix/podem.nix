0d6ab6098c66dc0a1a140ab8c04bc99c77761d4c
Mohamed Gaber <me@donn.website>
2024-10-23 17:49:02 +0300

    Update nix-eda, nl2bench (#52)
    * update nix-eda to `0814aa6c`
    * update nl2bench to 0.5.0 -- new pyosys-based version
    * add experimental support for podemquest
    Co-authored-by: Youssef Kandil <youssefkandil@aucegypt.edu>

--- nix/podem.nix
+++ nix/podem.nix
@@ -6,27 +6,27 @@
 gccStdenv.mkDerivation {
   name = "nctu-ee-podem";
   version = "0.1.0";
-  
+
   src = fetchFromGitHub {
     owner = "donn";
     repo = "VLSI-Testing";
     rev = "ff82db776521b294d79d54acc00b7b6eaaa5846d";
     sha256 = "sha256-Nj8hQb9XlRjIIrfht8VNEfORmwtb+WWrP6UVlWgo81A=";
   };
-  
+
   postPatch = ''
     sed -i 's/^LIBS.*/LIBS = /' podem/Makefile
   '';
-  
+
   buildPhase = ''
     make -C podem
   '';
-  
+
   installPhase = ''
     mkdir -p $out/bin
     cp podem/atpg $out/bin/atpg-podem
   '';
-  
+
   meta = with lib; {
     description = "A C++ implementation of PODEM used in the testing course of the NCTU EE program";
     homepage = "https://github.com/cylinbao/VLSI-Testing";
